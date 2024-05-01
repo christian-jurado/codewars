b0c1c2a7d0d299d859dcec76b031f8432baf9076
Christian Jurado <christian.jurado.n@gmail.com>
2024-05-01 11:17:10 -0600

    Update twitterSentimentalAnalysis.py

--- twitterSentimentalAnalysis.py
+++ twitterSentimentalAnalysis.py
@@ -1,11 +1,11 @@
 import tweepy
 from textblob import TextBlob
 
-consumer_key = 'L7eMpqlvOzU2xu92A3pejtHtu'
-consumer_secret = '2frbkDgynEVs2uASmqdDvtaky3WTAd9pbY0rv0nILKfZbfBV1m'
+consumer_key = 'xxxxx'
+consumer_secret = 'xxxxx'
 
-acces_token = '1769519338422239232-V89vSnp9QU8XPjmcM03chIy10wGNvC'
-acces_token_secret = 'dmYlaCj0ufhO1F7ChZ4uPXrtdVGXYudwRGyFjC7cvQCqp'
+acces_token = 'xxxxx'
+acces_token_secret = 'xxxxx'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(acces_token, acces_token_secret)
